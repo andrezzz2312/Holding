05bd6bdd4a4a80a55330da442ff14cf3bee0f8cd
Andrez Ramirez <andrezzz2312@gmail.com>
2022-04-16 01:54:47 -0500

    test2

--- main.js
+++ main.js
@@ -161,11 +161,9 @@ compactFP_button.addEventListener('click', function (e) {
           loop.style.zIndex = '-5'
           loop.classList.remove('short-vanish')
           loop.load()
+
           setTimeout(() => {
             loop.style.zIndex = '-1'
-          }, 300)
-          Setup()
-          setTimeout(() => {
             video1.remove()
             video2.remove()
             video3.remove()
